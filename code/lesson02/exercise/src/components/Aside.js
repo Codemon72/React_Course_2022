ca2af278b0a0ea35176426f0af4f5b00ad557921
Clemens Bruesch <clemensbruesch@gmail.com>
2022-11-18 20:32:42 +0100

    MenuItems read out by map function

--- code/lesson02/exercise/src/components/Aside.js
+++ code/lesson02/exercise/src/components/Aside.js
@@ -7,24 +7,13 @@ const Aside = ({numberOfMessages}) => {
   return (
     <aside className='w-1/4 h-full border-slate-400 border-r p-4'>
           <ul className='flex flex-col gap-2 font-semibold'>
-            {menu.map(menuItem => {
-              <MenuItem title={menuItem.title} href={menuItem.href} count={menuItem.count} />
-            })}
-            {[<li>
-              <a className='text-slate-700 hover:text-slate-900' href='#'>
-                Home
-              </a>
-            </li>,
-            <li>
-              <a className='text-slate-700 hover:text-slate-900' href='#'>
-                About me
-              </a>
-            </li>,
-            <li>
-              <a className='text-slate-700 hover:text-slate-900' href='#'>
-                Inbox
-              </a><span>&nbsp; ({numberOfMessages} messages)</span>
-            </li>]}
+            {menu.map(menuItem => 
+              (<MenuItem
+                title={menuItem.title}
+                href={menuItem.href}
+                count={menuItem.count}
+              />)
+            )}
           </ul>
         </aside>
   )
